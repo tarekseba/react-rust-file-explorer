927637a83ba733c00ceb465773053e9bcbd9e157
Tarek.Seba <tarek.seba@outlook.fr>
2023-07-01 23:35:30 +0200

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": ["./tsconfig.json"]
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "no-console": 1,
+      "quotes": "off",
+      "@typescript-eslint/quotes": [1, "double"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "react/jsx-uses-react": "off"
+
+    }
+}
